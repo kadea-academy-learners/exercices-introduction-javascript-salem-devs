b4df976a39ef01e27e57a6ed3b5e75ecb70295e6
Ben-Salem <salemko801@gmail.com>
2025-09-25 12:41:14 -0700

    Resolutio de l exercice 5

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,11 @@
-// Exercice 5
-// Écrivez votre code ici
+// Définition de la fonction
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+// Exemple d'utilisation
+let tempC = 25;
+console.log(tempC + "°C correspond à " + celsiusToFahrenheit(tempC) + "°F");
+
+// Export sous forme d'objet pour Jest
+module.exports = { celsiusToFahrenheit };
