94238f24fdecf1eacba3c765061e920271f10486
Ben-Salem <salemko801@gmail.com>
2025-09-25 13:49:18 -0700

    Resolution de l'execice 6

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,2 +1,9 @@
-// Exercice 6
-// Écrivez votre code ici
+// Déclaration des variables
+let age = 30;                // numérique
+let nom = "sam";           // chaîne de caractères
+let estConnecte = true;      // booléen
+let valeurNulle = null;      // null
+let inconnue;                // indéfinie
+
+// Export de toutes les variables pour que Jest puisse les lire
+module.exports = { age, nom, estConnecte, valeurNulle, inconnue };
