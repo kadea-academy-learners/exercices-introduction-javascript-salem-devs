e4c99546063e164cb927328f1635e97c1fcba6b6
Ben-Salem <salemko801@gmail.com>
2025-09-25 13:53:43 -0700

    Resolustion de l'exercice 7

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,10 @@
 // Exercice 7
 // Écrivez votre code ici
+// Déclaration des variables
+let ageEnfant = 10;                   
+let agePere = ageEnfant * 2;          // le double de l'enfant
+let ageMere = agePere - 5;            // âge du père moins 5
+let ageGrandPere = (ageMere * 2) + (ageEnfant / 2); // double de la mère + moitié de l'enfant
+let ageOncle = agePere + 10;          // âge du père plus 10
+
+module.exports = { ageEnfant, agePere, ageMere, ageGrandPere, ageOncle };
