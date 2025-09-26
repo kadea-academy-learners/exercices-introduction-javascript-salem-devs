4049106af2c969ffa77b5d92435f5f9c96664a41
Ben-Salem <salemko801@gmail.com>
2025-09-25 19:22:44 -0700

    Resolution de l'exercice 9

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,2 +1,36 @@
 // Exercice 9
 // Écrivez votre code ici
+
+let maisonFamiliale = 60000000;
+let terrains = 40000000;
+let liquidites = 20000000;
+let totalHeritage = maisonFamiliale + terrains + liquidites;
+
+partDesEnfants = totalHeritage * 0.75;
+partDuConjointEtFreres = totalHeritage * 0.25;
+
+let paul = partDesEnfants / 3;
+let marie = partDesEnfants / 3;
+let eric = (partDesEnfants / 3) / 2;
+let clair = (partDesEnfants / 3) / 2;
+let madameMukuna = partDuConjointEtFreres / 3;
+let joseph = partDuConjointEtFreres / 3;
+let sarah = partDuConjointEtFreres / 3;
+console.log("Héritage de Paul: " + paul + " CDF");
+console.log("Héritage de Marie: " + marie + " CDF");
+console.log("Héritage de Éric: " + eric + " CDF");  
+console.log("Héritage de Claire: " + clair + " CDF");
+console.log("Héritage de Madame MUKUNA: " + madameMukuna + " CDF");
+console.log("Héritage de Joseph: " + joseph + " CDF");
+console.log("Héritage de Sarah: " + sarah + " CDF");
+
+
+module.exports = {
+  paul,
+  marie,
+    eric,
+    clair,
+    madameMukuna,
+    joseph,
+    sarah,
+};
