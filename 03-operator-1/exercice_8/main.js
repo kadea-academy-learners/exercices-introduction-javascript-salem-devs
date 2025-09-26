cd01100a2048e6276852aba0915c2d7e7f695bdc
Ben-Salem <salemko801@gmail.com>
2025-09-25 18:38:21 -0700

    Resoulution de l'exercice 8

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,20 @@
 // Exercice 8
 // Écrivez votre code ici
+
+let salaireMensuel = 500;
+let loyer = salaireMensuel * 0.30;
+let nourriture = salaireMensuel * 0.20;
+let transport = salaireMensuel * 0.10;
+let autresDepenses = 50;
+autresDepenses += 25;
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+let reste = salaireMensuel - totalDepenses; 
+module.exports = {
+  salaireMensuel,
+  loyer,
+  nourriture,
+  transport,
+  autresDepenses,
+  totalDepenses,
+  reste,
+};
